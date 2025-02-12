ce011ab571af96d1ff8d1c628eff696f86ec0d90
marsi <yakshina.m@sch2009.net>
2025-02-12 16:14:15 +0300

    progress

--- main.py
+++ main.py
@@ -2,18 +2,35 @@ import sys
 import random
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QPainter, QPen, Qt
+from PyQt6.QtGui import QPainter, QColor
+
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
-    def run(self):
-        painter = QPainter()
-        painter.setPen(QPen("yellow"))
-        painter.drawEllipse(40, random.randint(0, 400), 400, 400)
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor("yellow"))
+        size = random.randint(10, 400)
+        x = random.randint(10, 400)
+        y = random.randint(10, 400)
+        qp.drawEllipse(x, y, size, size)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
